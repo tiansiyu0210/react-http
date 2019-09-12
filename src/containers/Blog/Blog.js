d1ec1208fb39f29e1417a1f8cf1dab4f7c5abe54
tiansiyu0210 <tiansiyu297@gmail.com>
2019-09-12 12:48:22 -0400

    223. Switching Between Pages

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 //import axios from 'axios';
 import axios from '../../axios';
 
-import FullPost from './FullPost/FullPost';
 import NewPost from './NewPost/NewPost';
 import './Blog.css';
 import Posts from './Posts/Posts';
@@ -17,20 +16,12 @@ class Blog extends Component {
                     <nav>
                         <ul>
                             <li><a href="/">Home</a></li>
-                            <li><a href="/new post">New Post</a></li>
+                            <li><a href="/new-post">New Post</a></li>
                         </ul>
                     </nav>
                 </header>
-                <Posts/>
-                <Route path="/" exact component={Posts}/>
-                {/*<Route path="/" exact render={() => <h1>Home 1</h1>}/>*/}
-                {/*<Route path="/"  render={() => <h1>Home 2</h1>}/>*/}
-                {/*<section>*/}
-                {/*    <FullPost id={this.state.selectedPostId}/>*/}
-                {/*</section>*/}
-                {/*<section>*/}
-                {/*    <NewPost />*/}
-                {/*</section>*/}
+                <Route path="/" exact component={Posts} />
+                <Route path="/new-post" component={NewPost}/>
             </div>
         );
     }
